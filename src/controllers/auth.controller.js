12c01deaf6853b5c7cda976e75b0cda615847f13
ColmanNicolas <busimaximiliano@gmail.com>
2024-05-29 17:12:51 -0300

    creacion de encriptado de pass y token de usuario

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,26 +1,44 @@
 import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+import createAccessToken from "../libs/jwt.js"
 
+
+//FUNCIONALIDAD PARA REGISTRAR
 export const register = async (req, res) => {
   
   const {email, password, username} = req.body
 
   try {
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       email,
-      password,
+      password: passwordHash,
       username
     })
     
     const userSaved = await newUser.save();
-    res.json(userSaved)
+    const token = await createAccessToken({id: userSaved._id})
+    
+    res.cookie("token", token)
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAd: userSaved.createdAt,
+      updatedAd: userSaved.updatedAt
+    })
+    
+    
+  
   } catch (error) {
-    console.log(error)
+    res.status(500).json({message: "error.message"})
   }
-
-
-  res.send("registrando!")
 }
 
+
+//FUNCIONALIDAD PARA LOGUEAR
 export const login = (req, res) => {
   res.send("login")
 }
