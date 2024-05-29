2afcc307daf49a363353de4a98abc19d779ec255
ColmanNicolas <busimaximiliano@gmail.com>
2024-05-29 17:47:31 -0300

    se crea funcionalidad para desloguear

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -70,3 +70,11 @@ export const login = async (req, res) => {
     res.status(500).json({message: "error.message"})
   }
 }
+
+//FUNCIONALIDAD PARA DESLOGUEAR
+export const logout = (req, res) => {
+ res.cookie("token", "", {
+  expires: new Date(0)
+ })
+ return res.sendStatus(200)
+}
