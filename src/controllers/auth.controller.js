3b8173cab5f567c607e61fd369d2751221cab3ae
ColmanNicolas <busimaximiliano@gmail.com>
2024-06-05 16:10:19 -0300

    creacion de rutas protegidas con token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -78,3 +78,18 @@ export const logout = (req, res) => {
  })
  return res.sendStatus(200)
 }
+
+//FUNCIONALIDAD PARA VALIDAR EL TOKEN
+export const profile = async (req, res) => {
+ const userFound = await User.findById(req.user.id)
+
+ if(!userFound) return res.status(400).json({message: "usuario no encontrado :("})
+
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+    createdAt: userFound.createdAt,
+    updatedAt: userFound.updatedAt,
+  })
+}
